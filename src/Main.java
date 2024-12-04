149ae264938c2ba17bf8a9b90663e53a57a3d211
rosieomusic <rosieomusic@gmail.com>
2024-12-04 14:28:25 -0500

    completed new problem

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -34,5 +35,26 @@ public class Main {
         }
         return total;
     }
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        int alice = 0;
+        int bob = 0;
+        for (int i = 0; i < 3; i++) {
+            if(a.get(i) > b.get(i)){
+                alice++;
+            } else if (a.get(i) < b.get(i)){
+                bob++;
+            }
+
+
+        }
+        List<Integer> result = new ArrayList<>();
+        result.add(alice);
+        result.add(bob);
+
+        return result;
+
+    }
+
+}
 
 }
